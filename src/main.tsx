e08efa2a1c786356477b8fa4254a8cd354faa50e
Adithya17-star <adithya1768@gmail.com>
2025-12-18 20:08:18 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import App from './App';
 import './index.css';
 import * as React from 'react';
 
@@ -9,3 +9,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
