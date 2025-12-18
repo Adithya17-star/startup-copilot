565255c1758c7b77744382e19bb5940dd7f82556
Adithya17-star <adithya1768@gmail.com>
2025-12-18 20:11:16 +0530

    Fix import typo and update class names in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Layout } from './components/Layout';
-import { IdeaInput } from './components/Idealnput'; // Ensure the file './components/IdeaInput.tsx' exists
+// FIXED: Typo in filename import
+import { IdeaInput } from './components/IdeaInput'; 
 import { ResultsContainer } from './components/ResultsContainer';
 import { InspirationSidebar } from './components/InspirationSidebar';
 import { StartupContextProvider } from './context/StartupContext';
@@ -8,14 +9,14 @@ import { StartupContextProvider } from './context/StartupContext';
 function App() {
   return (
     <StartupContextProvider>
-      <div className="flex min-h-screen bg-gray-50">
+      <div className="flex min-h-screen bg-slate-900 text-slate-50">
         <InspirationSidebar />
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col relative z-0">
           <Layout>
-            <>
+            <div className="animate-in fade-in zoom-in duration-500 slide-in-from-bottom-4">
               <IdeaInput />
               <ResultsContainer />
-            </>
+            </div>
           </Layout>
         </div>
       </div>
